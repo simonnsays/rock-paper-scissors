9e783fc694f9d58f8e39ce77505c517077ca8590
Darel <johndarel0914@gmail.com>
2025-12-11 20:31:48 +0800

    Add scoring system and declare winner

--- script.js
+++ script.js
@@ -15,9 +15,54 @@ function getHumanChoice() {
         return 'rock'
     } else if(answer === 'p') {
         return 'paper'
-    } else if(answer === s) {
-        return'scissor'
+    } else if(answer === 's') {
+        return'scissors'
     }
+} 
+
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+
+    function playRound(humanChoice, computerChoice) {
+        const humanAnswer = humanChoice.toLowerCase()
+        const computerAnswer = computerChoice.toLowerCase()
+        console.log(humanAnswer, computerAnswer)
+
+        if(humanAnswer === 'rock' && computerAnswer === 'scissors' ||
+            humanAnswer === 'scissors' && computerAnswer === 'paper' ||
+            humanAnswer === 'paper' && computerAnswer === 'rock'
+        ) {
+            humanScore++
+            alert("You Win! " + humanAnswer + " beats " + computerAnswer)
+        } else if(humanAnswer === computerAnswer) {
+            alert("It's a draw")
+        } else {
+            computerScore++ 
+            alert("You lose! " + computerAnswer + " beats " + humanAnswer) 
+        }
+    }
+
+
+    for(let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+
+        playRound(humanSelection, computerSelection)
+    }
+
+    if(humanScore > computerScore) {
+        console.log("You win!")
+    } else if(humanScore === computerScore) {
+        console.log("Draw")
+    } else {
+        console.log("Computer Wins..")
+    }
+    console.log(`score:
+    Human: ${humanScore}
+    Computer: ${computerScore}
+    `)
 }
 
-console.log(getHumanChoice())
+
+playGame()
